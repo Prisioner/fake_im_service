0bd373f935ab92c4aa0ce9de1c68d56b6f3e3827
Aleksandr Klimenkov <prisioner@mail.ru>
2018-02-28 00:22:10 +0300

    change locale

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,5 @@ module FakeImService
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.i18n.default_locale = :ru
   end
 end
--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -2,10 +2,10 @@ Apipie.configure do |config|
   config.app_name                = "Fake IM Service"
   config.api_base_url            = "/api"
   config.doc_base_url            = "/apipie"
-  config.default_locale = 'ru'
-  config.languages = ['ru']
+  config.default_locale = 'en'
+  config.languages = ['en']
 
-  config.app_info['1.0'] = 'Описание методов для API'
+  config.app_info['1.0'] = 'API documentation'
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
   # config.authenticate = Proc.new do
